fcb66e498478d97557ab570f2c037123d998f312
JoshuaHolloway <jhollow6@asu.edu>
2022-04-20 18:58:12 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
       <button onClick={() => setOpen(true)}>Open Modal</button>
       <Modal show={open} setShow={setOpen} />
       <Backdrop show={open} />
-      <div>branch-1--css-animation</div>
+      <div>branch-1--css-animation (updated via local copy)</div>
     </div>
   );
 
